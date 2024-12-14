0d36f015171d9c1cf94651df7fcd1e299d2083e9
Anthony Shellman <atshellman@gmail.com>
2024-12-14 16:15:14 -0700

    Remove various console logs

--- src/database/migrations-runner.ts
+++ src/database/migrations-runner.ts
@@ -7,24 +7,19 @@ import { dirname } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const migrationsDir = path.join(__dirname, 'migrations');
-console.log("Line 10", migrationsDir)
+
 // Function to load all migration files
 const loadMigrations = async () => {
   const files = await readdir(migrationsDir);
-  console.log("Line 14", files)
   return files.filter((file) => file.endsWith('.ts') || file.endsWith('.js'));
 };
 
 // Function to run migrations
 const runMigrations = async (direction: 'up' | 'down') => {
   const migrations = await loadMigrations();
-  console.log('Loaded migrations:', migrations);
   for (const migration of migrations) {
     const { [direction]: migrationFn } = await import(path.join(migrationsDir, migration));
     if (typeof migrationFn === 'function') {
-      console.log(`${direction} migration: ${migration}`);
-      console.log(`Starting migration: ${migration}`);
-
       try {
         await migrationFn(db);
         console.log(`Migration succeeded: ${migration}`)
@@ -47,9 +42,10 @@ const runMigrations = async (direction: 'up' | 'down') => {
       throw new Error('Please provide "up" or "down" as an argument.')
     }
 
-    await runMigrations(direction); // Change to 'down' for rollback
+    await runMigrations(direction)
   } catch (error) {
-    console.error('Error running migrations:', error);
+    console.error('Error running migrations:', error)
+    throw error
   } finally {
     await db.destroy();
   }
